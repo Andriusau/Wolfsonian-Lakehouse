eaeba94bb1b1eb2d1e89497d82a9b72c1aeaf2ee
Andrius A <andrius.aukstuolis@fiu.edu>
2026-08-26 14:23:33 +0000

    Blocking web spiders from API server

--- frontend-explorer/src/app/robots.ts
+++ frontend-explorer/src/app/robots.ts
@@ -2,10 +2,16 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: ['Baiduspider', 'Sogou web spider'],
+        disallow: '/',
+      },
+    ],
     sitemap: 'https://lakehouse.wolfsonian.org/sitemap.xml',
   };
 }
